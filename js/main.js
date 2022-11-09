155a4742d7dcf4fb0051cda25226271e5166db7e
J-Massoda <emilienmassoda@gmail.com>
2022-11-09 21:34:43 +0100

    navbar sorted out

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
+// /* Open when someone clicks on the span element */
+// function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+// }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
+// /* Close when someone clicks on the "x" symbol inside the overlay */
+// function closeNav() {
+//     document.getElementById("myNav").style.width = "0%";
+// }
+// Access the hamburger and nav-menu classes.
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+
+// Add an event listener to the hamburger class so when the user clicks on it, the menu
+//  displays. If the menu is already displayed, it will simply hide it from view.
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+// Also add an event listener to each of the page links. This way the menu goes into hiding when the user clicks on a link.
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
 
 // Slide show ........................................................................
 let slideIndex = 0;
